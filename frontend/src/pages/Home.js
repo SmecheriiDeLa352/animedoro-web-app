6afa951fc7afeef7dc2a879f10f7f946df2484ad
Anca Ivancescu <anca.ivancescu@customsoft.ro>
2021-05-25 17:38:12 +0300

    scos commit  Revert "Timer added"
    This reverts commit 2bf518df7cbdd682850458124289e691d391f785.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-// import {Navbar, Nav, NavDropdown, Button} from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Button} from 'react-bootstrap'
 import naruto from "../images/naruto.png"
 import plus from "../images/plus.png"
 import minus from "../images/minus.png"
 import styled from "styled-components";
+import axios from "axios";
 
 const ButtonStart = styled.a`
 position: absolute;
@@ -26,67 +27,45 @@ line-height: 70px;
 color: #FFAA15;
 
 `
-const minute = 60
 class Home extends React.Component{
   constructor(props) {
-     super(props);
-     this.state = {
-       leftTime : 20 * minute,
-       running : false
-     }
-
+    super(props);
+    this.state = {
+      users: [],
+    }
   }
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("/api/user/")
+      .then((res) => this.setState({ users: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+
 
   imageClick = () => {
     console.log('Click!!!!');
  }
- addTime = ()=> {
-   this.setState({
-     leftTime : this.state.leftTime + minute
-   })
- }
- substractTime = ()=> {
-  this.setState({
-    leftTime : this.state.leftTime - minute
-  })
-}
-afisFrumos= ()=>{
-  return Math.floor( this.state.leftTime / minute) + " : " + ("0" + (this.state.leftTime % minute)).slice(-2)
-
-
-}
-startStop= () =>{
-  if ( this.state.running === false){
-    this.setState({
-      running : true
-    })
-    this.clockInterval = setInterval(() => {
-      this.setState({
-        leftTime : this.state.leftTime - 1
-      })
-    }, 1000);
-  }
-  else {
-    this.setState({
-      running : false
-    })
-    clearInterval(this.clockInterval)
-  }
-}
   render() {
-      return (<div className="MainPage">
-       <img className="plus" onClick={this.addTime} src={plus} />
+      return (<div className="MainPage">      
+       <img className="plus" onClick={this.imageClick} src={plus} />
        <img className="naruto" src={naruto} alt>
        </img>
-       <ButtonStart  onClick={this.startStop} primary>{this.state.running? "Stop" : "Start"}</ButtonStart>
-
-       <img className="minus" onClick={this.substractTime} src={minus} />
-        {this.afisFrumos()}
-       </div>
-
-
-
-
+       <ButtonStart primary>START</ButtonStart>
+       
+       <img className="minus" onClick={this.imageClick} src={minus} />
+      
+      { // this was a test
+      this.state.users.forEach(user => {
+        console.log(user.email)
+      })
+      }
+       </div>    
       )
   }
 }
