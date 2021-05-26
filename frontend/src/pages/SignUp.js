4ebd6fdb166ed9be57f256e1b375eca48d4410b7
dianapaula19 <dianabacircea19@gmail.com>
2021-05-26 21:29:24 +0300

    Created the SignUp page

---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,64 @@
+import React from "react";
+import axios from "axios"
+import querystring from "querystring"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+
+
+export default function SignUp(props) {
+
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = data => {
+    axios.post('api/user/', {
+        username: data.username,
+        password: data.password,
+        email: data.email,
+        f_name: data.firstName,
+        l_name: data.lastName,
+        calendar_id: 1
+
+    }).catch((err) => {
+        console.error(err)
+    })
+  }
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label> 
+          First Name:
+          <br/>
+          <input type="text" {...register("firstName")} required/>
+        </label>
+        <br/>
+        <label> 
+          Last Name:
+          <br/>
+          <input type="text" {...register("lastName")} required/>
+        </label>
+        <br/>
+        <label> 
+          Email:
+          <br/>
+          <input type="email" {...register("email")} required/>
+        </label>
+        <br/>
+        <label> 
+          Username:
+          <br/>
+          <input type="text" {...register("username")} required/>
+        </label>
+        <br/>
+        <label>
+          Password:
+          <br/>
+          <input type="password" {...register("password")} required/>
+        </label>
+        <br/>
+        <input type="submit" value="Sign Up"/>
+      </form>
+    </div>
+  )
+}
